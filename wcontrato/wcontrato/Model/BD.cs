e9db1501d8d6bd48653c32e2ca2b6c326bb04330
Weber Sistemas de Gestão <70588661+weberinfo@users.noreply.github.com>
2025-05-13 14:31:26 -0300

    Ajustado para compatibilidade com Firebird 4
    #adicionar linhas abaixo no Firebird.conf
    
    AuthServer = Legacy_Auth, Srp
    UserManager = Legacy_UserManager, Srp

--- wcontrato/wcontrato/Model/BD.cs
+++ wcontrato/wcontrato/Model/BD.cs
@@ -14,22 +14,10 @@ namespace wcontrato.Model
         public static string _conectionString;
         public BD()
         {
-            _conectionString =
-                    "User=SYSDBA;" +
-                    "Password=masterkey;" +
-                    "Database=" + Globals.dbPath + ";" +
-                    "ConnectionTimeout=0;" +
-                    "DataSource=localhost;" +
-                    "Port=3050;" +
-                    "Dialect=3;" +
-                    "Charset=ISO8859_1;" +
-                    "Role=;" +
-                    "Connection lifetime=15;" +
-                    "Pooling=true;" +
-                    "MinPoolSize=0;" +
-                    "MaxPoolSize=50;" +
-                    "Packet Size=8192;" +
-                    "ServerType=0";
+
+
+            _conectionString = "User=SYSDBA;Password=masterkey;Database=" + Globals.dbPath + ";DataSource=localhost;Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=1;Pooling=true;MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;AuthPlugins=Legacy_Auth;";
+
         }
         public static FbConnection conectaBd()
         {
